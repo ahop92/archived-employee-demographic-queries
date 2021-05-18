fbc11873f75a9f11369d61ec548997dfa0dc6cb4
ahop92 <80172357+ahop92@users.noreply.github.com>
2021-05-17 22:16:30 -0400

    Adding updated README.

---
+++ EmployeeSQL/QuickDBD-sql-challenge.sql
@@ -0,0 +1,180 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/SHwJ7c
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+DROP TABLE IF EXISTS "Department_Manager";
+DROP TABLE IF EXISTS "Department_Employee";
+DROP TABLE IF EXISTS "Departments";
+DROP TABLE IF EXISTS "Salaries";
+DROP TABLE IF EXISTS "Employees";
+DROP TABLE IF EXISTS "Titles";
+
+
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+
+CREATE TABLE "Employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title" VARCHAR(10)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(5)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Department_Employee" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Department_Employee" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "Department_Manager" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_Department_Manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Department_Employee" ADD CONSTRAINT "fk_Department_Employee_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Department_Employee" ADD CONSTRAINT "fk_Department_Employee_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Department_Manager" ADD CONSTRAINT "fk_Department_Manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Department_Manager" ADD CONSTRAINT "fk_Department_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+SELECT * FROM "Employees";
+SELECT * FROM "Salaries";
+SELECT * FROM "Department_Employee";
+SELECT * FROM "Department_Manager";
+SELECT * FROM "Departments";
+SELECT * FROM "Titles";
+
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT 
+"Employees".emp_no,
+"Employees".last_name,
+"Employees".first_name,
+"Employees".sex,
+"Salaries".salary
+FROM "Employees"
+INNER JOIN "Salaries" ON 
+"Employees".emp_no = "Salaries".emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+-- Ref: https://www.sqlservercentral.com/forums/topic/using-wildcard-with-a-date-column
+SELECT 
+"Employees".first_name,
+"Employees".last_name,
+"Employees".hire_date
+FROM "Employees"
+WHERE "Employees".hire_date >= '1986-01-01' AND "Employees".hire_date <= '1986-12-31';
+
+
+-- List the manager of each department with the following information: department number, department name, 
+-- the manager's employee number, last name, first name.
+SELECT 
+"Department_Manager".dept_no,
+"Departments".dept_name,
+"Department_Manager".emp_no, 
+"Employees".last_name,
+"Employees".first_name
+FROM "Department_Manager"
+INNER JOIN "Departments" ON 
+"Department_Manager".dept_no = "Departments".dept_no
+INNER JOIN "Employees" ON
+"Department_Manager".emp_no = "Employees".emp_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT
+"Department_Employee".emp_no,
+"Employees".last_name,
+"Employees".first_name,
+"Departments".dept_name
+FROM "Department_Employee"
+INNER JOIN "Employees" ON 
+"Department_Employee".emp_no = "Employees".emp_no
+INNER JOIN "Departments" ON
+"Department_Employee".dept_no = "Departments".dept_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- Ref: https://www.w3schools.com/sql/sql_wildcards.asp
+SELECT 
+"Employees".first_name,
+"Employees".last_name,
+"Employees".sex
+FROM "Employees"
+WHERE "Employees".first_name = 'Hercules' AND "Employees".last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT
+"Department_Employee".emp_no,
+"Employees".last_name,
+"Employees".first_name,
+"Departments".dept_name
+FROM "Department_Employee"
+INNER JOIN "Employees" ON 
+"Department_Employee".emp_no = "Employees".emp_no
+INNER JOIN "Departments" ON
+"Department_Employee".dept_no = "Departments".dept_no
+WHERE "Departments".dept_name = 'Sales'; 
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT
+"Department_Employee".emp_no,
+"Employees".last_name,
+"Employees".first_name,
+"Departments".dept_name
+FROM "Department_Employee"
+INNER JOIN "Employees" ON 
+"Department_Employee".emp_no = "Employees".emp_no
+INNER JOIN "Departments" ON
+"Department_Employee".dept_no = "Departments".dept_no
+WHERE "Departments".dept_name = 'Sales' OR "Departments".dept_name = 'Development'; 
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT  
+"Employees".last_name, 
+COUNT("Employees".last_name) AS "Last Name Count"
+FROM "Employees"
+GROUP BY "Employees".last_name
+ORDER BY "Last Name Count" DESC;
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # sql-challenge
+
+## Background
+
+The goal of this SQL analysis is to profile information about employees of the HP corporation from the 1980s and 1990s. The only remaining information on the employees from that era is contained in 6 separate CSV files.
+
+The project was approached using two priamry approaches: 
+
+### Data Engineering
+
+The https://app.quickdatabasediagrams.com/#/ website tool was used to visualize the overall structure of the database. The data engineering consisted of manually evaluating each csv to identify the keys for each table and translating that to the corresponding structures in PostgreSQL. Lastly, primary and foreign keys were established given the common data shared among the different CSVs. 
+
+### Data Analysis
+
+
+SQL queries summarize the data via two new summary tables inserted into each worksheet with the following categories: 
+
+Table 1: List of basic details of each employee including employee number, last name, first name, sex, and salary.
+
+Table 2: List including first name, last name, and hire date for employees who were hired in 1986.
+
+Table 3: List of the manager of each department including their department number, department name, the manager's employee number, last name, first name.
+
+Table 4: List the department of each employee including employee number, last name, first name, and department name.
+
+Table 5: List of the first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+Table 6: List of all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+Table 7: List of all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+Table 8: In descending order, identifying how many employees share the same last name.
+
+
