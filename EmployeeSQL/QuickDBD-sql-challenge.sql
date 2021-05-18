f7aed16f9dcc5afa6c65311db77ee6253df9236b
ahop92 <80172357+ahop92@users.noreply.github.com>
2021-05-17 22:25:52 -0400

    Updated QuickDBD code with comments and code exclusively creating table schemata.

--- EmployeeSQL/QuickDBD-sql-challenge.sql
+++ EmployeeSQL/QuickDBD-sql-challenge.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS "Salaries";
 DROP TABLE IF EXISTS "Employees";
 DROP TABLE IF EXISTS "Titles";
 
-
+-- Creating Titles Table
 CREATE TABLE "Titles" (
     "title_id" VARCHAR(10)   NOT NULL,
     "title" VARCHAR(30)   NOT NULL,
@@ -19,6 +19,7 @@ CREATE TABLE "Titles" (
 );
 
 
+-- Creating Employees Table
 CREATE TABLE "Employees" (
     "emp_no" INT   NOT NULL,
     "emp_title" VARCHAR(10)   NOT NULL,
@@ -32,6 +33,7 @@ CREATE TABLE "Employees" (
      )
 );
 
+-- Creating Departments Table
 CREATE TABLE "Departments" (
     "dept_no" VARCHAR(10)   NOT NULL,
     "dept_name" VARCHAR(30)   NOT NULL,
@@ -40,6 +42,7 @@ CREATE TABLE "Departments" (
      )
 );
 
+-- Creating Department Employee Table
 CREATE TABLE "Department_Employee" (
     "emp_no" INT   NOT NULL,
     "dept_no" VARCHAR(10)   NOT NULL,
@@ -48,6 +51,7 @@ CREATE TABLE "Department_Employee" (
      )
 );
 
+-- Creating Department Manager Table
 CREATE TABLE "Department_Manager" (
     "dept_no" VARCHAR(10)   NOT NULL,
     "emp_no" INT   NOT NULL,
@@ -56,6 +60,7 @@ CREATE TABLE "Department_Manager" (
      )
 );
 
+-- Creating Salaries Table
 CREATE TABLE "Salaries" (
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL,
@@ -63,6 +68,8 @@ CREATE TABLE "Salaries" (
         "emp_no"
      )
 );
+
+-- Adding constraings for the database in the form of primary, composite, and foreign keys
 
 ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title" FOREIGN KEY("emp_title")
 REFERENCES "Titles" ("title_id");
@@ -82,99 +89,3 @@ REFERENCES "Employees" ("emp_no");
 ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
 
-SELECT * FROM "Employees";
-SELECT * FROM "Salaries";
-SELECT * FROM "Department_Employee";
-SELECT * FROM "Department_Manager";
-SELECT * FROM "Departments";
-SELECT * FROM "Titles";
-
--- List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT 
-"Employees".emp_no,
-"Employees".last_name,
-"Employees".first_name,
-"Employees".sex,
-"Salaries".salary
-FROM "Employees"
-INNER JOIN "Salaries" ON 
-"Employees".emp_no = "Salaries".emp_no;
-
--- List first name, last name, and hire date for employees who were hired in 1986.
--- Ref: https://www.sqlservercentral.com/forums/topic/using-wildcard-with-a-date-column
-SELECT 
-"Employees".first_name,
-"Employees".last_name,
-"Employees".hire_date
-FROM "Employees"
-WHERE "Employees".hire_date >= '1986-01-01' AND "Employees".hire_date <= '1986-12-31';
-
-
--- List the manager of each department with the following information: department number, department name, 
--- the manager's employee number, last name, first name.
-SELECT 
-"Department_Manager".dept_no,
-"Departments".dept_name,
-"Department_Manager".emp_no, 
-"Employees".last_name,
-"Employees".first_name
-FROM "Department_Manager"
-INNER JOIN "Departments" ON 
-"Department_Manager".dept_no = "Departments".dept_no
-INNER JOIN "Employees" ON
-"Department_Manager".emp_no = "Employees".emp_no;
-
--- List the department of each employee with the following information: employee number, last name, first name, and department name.
-SELECT
-"Department_Employee".emp_no,
-"Employees".last_name,
-"Employees".first_name,
-"Departments".dept_name
-FROM "Department_Employee"
-INNER JOIN "Employees" ON 
-"Department_Employee".emp_no = "Employees".emp_no
-INNER JOIN "Departments" ON
-"Department_Employee".dept_no = "Departments".dept_no;
-
--- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
--- Ref: https://www.w3schools.com/sql/sql_wildcards.asp
-SELECT 
-"Employees".first_name,
-"Employees".last_name,
-"Employees".sex
-FROM "Employees"
-WHERE "Employees".first_name = 'Hercules' AND "Employees".last_name LIKE 'B%';
-
--- List all employees in the Sales department, including their employee number, last name, first name, and department name.
-SELECT
-"Department_Employee".emp_no,
-"Employees".last_name,
-"Employees".first_name,
-"Departments".dept_name
-FROM "Department_Employee"
-INNER JOIN "Employees" ON 
-"Department_Employee".emp_no = "Employees".emp_no
-INNER JOIN "Departments" ON
-"Department_Employee".dept_no = "Departments".dept_no
-WHERE "Departments".dept_name = 'Sales'; 
-
--- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-SELECT
-"Department_Employee".emp_no,
-"Employees".last_name,
-"Employees".first_name,
-"Departments".dept_name
-FROM "Department_Employee"
-INNER JOIN "Employees" ON 
-"Department_Employee".emp_no = "Employees".emp_no
-INNER JOIN "Departments" ON
-"Department_Employee".dept_no = "Departments".dept_no
-WHERE "Departments".dept_name = 'Sales' OR "Departments".dept_name = 'Development'; 
-
--- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT  
-"Employees".last_name, 
-COUNT("Employees".last_name) AS "Last Name Count"
-FROM "Employees"
-GROUP BY "Employees".last_name
-ORDER BY "Last Name Count" DESC;
